2ae1bbb6cdaa62db1f9b4b595b9ee6f8636c5f31
mikica96 <57533511+mikica96@users.noreply.github.com>
2019-11-11 18:39:04 +0100

    Update proba.php

--- proba.php
+++ proba.php
@@ -7,17 +7,8 @@
 	
 	<?php 
 
-		$array=[5,43,5,6,7,7,7];
-
-		if (count($array) > 0 ) {
-			$sum=0;
-			foreach ($array as $key) {
-				$sum=$sum+$key;
-			}
-			echo $sum;
-		}else{
-			echo $sum=0;
-		}
+		echo "ovo je druga grana";
+		
 	 ?>
 </body>
 </html>
